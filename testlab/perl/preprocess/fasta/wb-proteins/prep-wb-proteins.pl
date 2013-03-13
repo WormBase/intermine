11a5b4df91d1cb80d755586b88bd2e5750228659
JD Wong <jdmswong@wormbase.org>
2013-03-13 12:39:47 -0400

    CDS.transcript, replaced by transcripts. is now a collection priorities validated
    WormMine: Data validated for AceDB CDS.
    
    WormMine-dev: loads protein fasta data, not merged
    
    close #867

--- testlab/perl/preprocess/fasta/wb-proteins/prep-wb-proteins.pl
+++ testlab/perl/preprocess/fasta/wb-proteins/prep-wb-proteins.pl
@@ -5,9 +5,9 @@ use strict;
 my ($infile_path,$outfile_path) = @ARGV;
 
 open( my $infile, $infile_path) or die $infile_path.": $!";
-open( my $outfile, '>'.$outfile_path) or die $outfile_path.": $!";
+open( my $outfile, '>'.$outfile_path) or die $outfile_path.": $!"; 
 
 while(<$infile>){
-	s[>(\S+).*$][>Protein:$1];
-	print $outfile $_;
+	s[>(\S+).*$][>Protein:$1]; 
+	print $outfile $_; 
 }
