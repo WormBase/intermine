14e1eb5574d534ed81fcec53a250e9990efcce3c
JD Wong <jdmswong@wormbase.org>
2013-03-27 12:06:52 -0400

    Protein purging script operational, takes whitelist generated by species list in wormbase.conf by gen_whitelist.pl

--- testlab/perl/preprocess/wb-acedb/protein/purge_protein.pl
+++ testlab/perl/preprocess/wb-acedb/protein/purge_protein.pl
@@ -3,19 +3,31 @@
 use strict;
 use XML::Simple qw(:strict);
 use Data::Dumper;
+use feature "state";
 
-my ($infile_path,$outfile_path, $rootname) = @ARGV;
+my (
+	$infile_path,
+	$outfile_path, 
+	$whitelist_path, 
+	$rejects_path
+) = @ARGV;
 
-die &usage if (scalar @ARGV) != 2;
+die &usage if (scalar @ARGV) != 4;
 
 open( my $infile, $infile_path) or die $infile_path.": $!";
 open( my $outfile, '>'.$outfile_path) or die $outfile_path.": $!";
+open( my $whitelist, $whitelist_path) or die $whitelist_path.": $!";
+open( my $rejectsfile, '>'.$rejects_path) or die $rejects_path.": $!";
+
+my %species_wl = ();
+while(<$whitelist>){
+	chomp;
+	$species_wl{$_} = 1;
+}
 
 my $xs = XML::Simple->new( 
 	ForceArray 	=> 1, 
 	KeyAttr 	=> {},
-	KeepRoot 	=> 1,
-	NoSort		=> 1
 );
 my $xml_chunk = "";
 while(<$infile>){
@@ -30,15 +42,46 @@ while(<$infile>){
 
 
 sub process{
+	state $count = 0;
+	$count++;
+	print $count."\n" if $count % 1000 == 0; 
+	
 	my $xml_chunk = shift;
-	print "[[processing item]]\n";
-	my $ref = $xs->XMLin($xml_chunk);
-	print Dumper $ref;
-	print $xs->XMLout($ref);
+	#print "[[processing item]]\n";
+	my $ref;
+	eval{
+		$ref = $xs->XMLin($xml_chunk);
+	};
+	if( $@ ){ # if eval had to catch
+		print $rejectsfile $xml_chunk."\n";
+	}
+	
+	my $id = $ref->{content};
+	return if $id =~ /^MSP:/;
+	
+	my $species = $ref->{Origin}[0]{Species}[0]{Species}[0];
+	return unless defined $species_wl{$species};
+	
+	#print Dumper $species;
+	#print $id."\n";
+	print $outfile $xml_chunk."\n";
+}
+
+sub status_report{
+	my $count = shift;
+	state $oom = 1; # order of magnitude
+	if($count < $oom*10){
+		print $count."\n";
+	}elsif($count = $oom){
+		$oom++;
+		print $count."\n";
+	}
 }
 
 sub usage{
 <<"USAGE";
-Usage: perl $0 <infile> <outfile> \n
+Usage: perl $0 <infile> <outfile> <whitelist> <rejectsfile>
+	whitelist: list of allowed species
+	rejects: file to dump XML records rejected by the parser
 USAGE
 }
