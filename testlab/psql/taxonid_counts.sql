3e9c93ce38848786ce92c948dd533de22f04ad6e
JD Wong <jdmswong@wormbase.org>
2013-06-10 16:25:10 -0400

    build updated to WS238, intermine v 1.2.1

--- testlab/psql/taxonid_counts.sql
+++
@@ -1,5 +0,0 @@
-SELECT name, t1.taxonid, counts.count
-from organism as t1 
-join (select taxonid,count(*) from organism group by taxonid having count(*) > 1) as counts
-    ON t1.taxonid = counts.taxonid
-;
