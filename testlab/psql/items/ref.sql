29718f2c9c6871e44f72878ffe21d1b54283904d
JD Wong <jdmswong@wormbase.org>
2013-08-28 11:46:42 -0400

    new redeployment build file added

--- testlab/psql/items/ref.sql
+++ testlab/psql/items/ref.sql
@@ -1,24 +1,24 @@
+-- joins items and their references
 SELECT 
 	i1.id, 
 	i1.identifier, 
 	i1.classname, 
 	a.intermine_value AS pID,
-	rl.name AS reflist, 
+	r.name AS reference, 
 	i2.id,
-	rl.refids,
+	r.refid,
 	a2.intermine_value AS pID
 	
 FROM 
 	item i1
-	JOIN referencelist rl ON i1.id = rl.itemid
-	JOIN item i2 ON rl.refids = i2.identifier
+	JOIN reference r ON i1.id = r.itemid
+	JOIN item i2 ON r.refid = i2.identifier
 	JOIN attribute a ON a.itemid = i1.id
 	JOIN attribute a2 ON a2.itemid = i2.id
 WHERE 
-		rl.name = 'CDSs'
-	AND a.name = 'primaryIdentifier'
+        a.name = 'primaryIdentifier'
 	AND a2.name = 'primaryIdentifier'
-	--AND i2.identifier = '4_192689'
+	AND i1.identifier = '11_409042'
 	--AND a.intermine_value = 'JA:JA51573'
 	--AND a.intermine_value = 'JA:JA65336'
 ;
