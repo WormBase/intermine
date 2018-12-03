4ee8ae69ea41282c2314f1e8c96e5d0635035b02
Paulo Nuin <nuin@genedrift.org>
2018-12-03 16:16:07 -0700

    protein.sh (deploy) updated

--- wormmine/support/scripts/deploy/split_deploy/protein_fasta.sh
+++ wormmine/support/scripts/deploy/split_deploy/protein_fasta.sh
@@ -6,23 +6,34 @@
 # TODO: not process XML files already processed
 
 #set the version to be accessed
-wbrel="WS265"
+wbrel="WS268"
 echo 'Release version' $wbrel
 
+declare -A species=(["c_elegans"]="PRJNA13758"
+                   ["b_malayi"]="PRJNA10729"
+                   ["c_angaria"]="PRJNA51225"
+                   ["c_brenneri"]="PRJNA20035"
+                   ["c_briggsae"]="PRJNA10731"
+                   ["c_japonica"]="PRJNA12591"
+                   ["c_remanei"]="PRJNA53967"
+                   ["c_tropicalis"]="PRJNA53597"
+                     ["o_volvulus"]="PRJEB513"
+                   ["p_pacificus"]="PRJNA12644"
+                   ["p_redivivus"]="PRJNA186477"
+                   ["s_ratti"]="PRJEB125"
+                   ["c_sinica"]="PRJNA194557")
 
-declare -A species=(["c_elegans"]="PRJNA13758")
+
+
+
+
 echo 'Deploying ' $species
 echo
-sourcedir='/mnt/data2/acedb_dumps/WS265/WS265-test-data'
-#sourcedir='/mnt/data2/acedb_dumps/'$wbrel'' # <---- XML dump location
 
-
-echo 'Source directory is at' $sourcedir
-echo
 intermine='/mnt/data2/wormmine'
 
-datadir=$intermine'/datadir_small'   # for now the datadir is inside the intermine directory
-#datadir=$intermine'/datadir'$wbrel''   # for now the datadir is inside the intermine directory
+# datadir=$intermine'/datadir_small'   # for now the datadir is inside the intermine directory
+datadir=$intermine'/datadir'$wbrel''   # for now the datadir is inside the intermine directory
 acexmldir=$datadir'/wormbase-acedb'
 testlab=$intermine'/wormmine/support/scripts/'
 compara=$intermine'/wormmine/support/compara'
@@ -36,11 +47,12 @@ for spe in "${!species[@]}"
 do
   echo species: $spe ${species["$spe"]}
 
+
   #################### get the protein data ####################
   echo 'Getting protein data'
-  mkdir -vp $datadir"/fasta/"$spe"/proteins/raw"
-  mkdir -vp $datadir"/fasta/"$spe"/proteins/prepped"
-  cd $datadir"/fasta/"$spe"/proteins/raw"
+  # mkdir -vp $datadir"/fasta/"$spe"/proteins/raw"
+  # mkdir -vp $datadir"/fasta/"$spe"/proteins/prepped"
+  # cd $datadir"/fasta/"$spe"/proteins/raw"
   if [ ! -f "$spe"."${species["$spe"]}"."$wbrel".protein.fa ]; then
     echo "$spe"."${species["$spe"]}"."$wbrel".protein.fa 'not found'
     echo 'transferring ' "$spe"."${species["$spe"]}"."$wbrel".protein.fa
@@ -50,8 +62,13 @@ do
     echo "$spe"."${species["$spe"]}"."$wbrel".protein.fa 'found, not transferring'
   fi
   echo 'Pre-processing protein FASTA file'
-  perl $testlab'/fasta/wb-proteins/prep-wb-proteins.pl' "$spe"."${species["$spe"]}"."$wbrel".protein.fa ../prepped/"$spe"."${species["$spe"]}"."$wbrel".protein.fa
-  echo 
+  # put prepped dir here
+  awk '{ if (NF > 3) {split($2,res,"="); print ">"res[2]} else {print}}' < "$spe"."${species["$spe"]}"."$wbrel".protein.fa > "$spe"."${species["$spe"]}"."$wbrel".protein.final.fa
+
+
+
+#   perl $testlab'/fasta/wb-proteins/prep-wb-proteins.pl' "$spe"."${species["$spe"]}"."$wbrel".protein.fa ../prepped/"$spe"."${species["$spe"]}"."$wbrel".protein.fa
+#   echo 
 
 done
 
