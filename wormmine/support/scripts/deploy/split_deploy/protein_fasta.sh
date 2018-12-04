02ecb4c46a8878de7cb426ffc978b7ea3f03ea5f
Paulo Nuin <nuin@genedrift.org>
2018-12-03 20:33:43 -0700

    protein_fasta deploy updated

--- wormmine/support/scripts/deploy/split_deploy/protein_fasta.sh
+++ wormmine/support/scripts/deploy/split_deploy/protein_fasta.sh
@@ -17,14 +17,11 @@ declare -A species=(["c_elegans"]="PRJNA13758"
                    ["c_japonica"]="PRJNA12591"
                    ["c_remanei"]="PRJNA53967"
                    ["c_tropicalis"]="PRJNA53597"
-                     ["o_volvulus"]="PRJEB513"
+                   ["o_volvulus"]="PRJEB513"
                    ["p_pacificus"]="PRJNA12644"
                    ["p_redivivus"]="PRJNA186477"
                    ["s_ratti"]="PRJEB125"
                    ["c_sinica"]="PRJNA194557")
-
-
-
 
 
 echo 'Deploying ' $species
@@ -54,7 +51,7 @@ do
   # mkdir -vp $datadir"/fasta/"$spe"/proteins/prepped"
   # cd $datadir"/fasta/"$spe"/proteins/raw"
   if [ ! -f "$spe"."${species["$spe"]}"."$wbrel".protein.fa ]; then
-    echo "$spe"."${species["$spe"]}"."$wbrel".protein.fa 'not found'
+    echo "$spe"."${species["$spe"]}"."$wbrel".protein.fa 'not downloaded, downloading'
     echo 'transferring ' "$spe"."${species["$spe"]}"."$wbrel".protein.fa
     wget -q --show-progress -O "$spe"."${species["$spe"]}"."$wbrel".protein.fa.gz "ftp://ftp.wormbase.org/pub/wormbase/releases/"$wbrel"/species/"$spe"/"${species["$spe"]}"/"$spe"."${species["$spe"]}"."$wbrel".protein.fa.gz"
     gunzip -v "$spe"."${species["$spe"]}"."$wbrel".protein.fa.gz
@@ -63,8 +60,13 @@ do
   fi
   echo 'Pre-processing protein FASTA file'
   # put prepped dir here
-  awk '{ if (NF > 3) {split($2,res,"="); print ">"res[2]} else {print}}' < "$spe"."${species["$spe"]}"."$wbrel".protein.fa > "$spe"."${species["$spe"]}"."$wbrel".protein.final.fa
+  # if [ $spe -eq "c_angaria" ];then
 
+    # awk '{ if (NF > 1) {split($2,res,"="); print ">"res[2]} else {print}}' < "$spe"."${species["$spe"]}"."$wbrel".protein.fa > "spe"."${species["$spe"]}"."$wbrel".protein.final.fa
+   
+  # else
+    awk '{ if (NF > 1) {split($2,res,"="); print ">"res[2]} else {print}}' < "$spe"."${species["$spe"]}"."$wbrel".protein.fa > "$spe"."${species["$spe"]}"."$wbrel".protein.final.fa
+  # fi
 
 
 #   perl $testlab'/fasta/wb-proteins/prep-wb-proteins.pl' "$spe"."${species["$spe"]}"."$wbrel".protein.fa ../prepped/"$spe"."${species["$spe"]}"."$wbrel".protein.fa
